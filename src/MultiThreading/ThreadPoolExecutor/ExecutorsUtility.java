29d8d35a2589b19eb8ad6135dc84ac1dbe99965e
LENOVO <LENOVO@192.168.1.36>
2025-04-16 21:03:25 +0530

    Executors Utility for diff type of thread pool

---
+++ src/MultiThreading/ThreadPoolExecutor/ExecutorsUtility.java
@@ -0,0 +1,34 @@
+package MultiThreading.ThreadPoolExecutor;
+
+import java.util.concurrent.*;
+
+public class ExecutorsUtility {
+	
+	public static void main(String args[])
+	{
+		// Fixed thread pool executor : fixed number of min/max thread
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		executor.submit(()->{
+			System.out.println("Fixed thread pool");
+		});
+		
+		// Cached thread pool executor : creates thread on demand 
+		ExecutorService executor1 = Executors.newCachedThreadPool();
+		executor1.submit(()->{
+			System.out.println("Cached thread pool");
+		});
+		
+		// Single thread pool executor: only 1 thread is present, good for sequential processing
+		ExecutorService executor2 = Executors.newSingleThreadExecutor();
+		executor2.submit(()->{
+			System.out.println("Single thread pool");
+		});
+		
+		// Fork-Join or Work-Stealing executor
+		ExecutorService executor3 = Executors.newWorkStealingPool();
+		executor3.submit(()->{
+			System.out.println("Fork-Join thread pool");
+		});
+	}
+
+}
